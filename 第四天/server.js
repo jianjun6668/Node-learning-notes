3c0424ca2e4935024b4ff7a45eab5af2dcc72f03
spring <jianjun6668@163.com>
2017-07-24 22:06:41 +0800

    第四天

---
+++ 第四天/server.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const bodyParaer=require('body-parser');
+const expressStatic=require('express-static');
+
+var server=express();
+server.listen(8082);
+//get 请求
+server.use('/',function(req,res){
+    console.log(req.query);
+});
+
+//post 请求
+// server.use(bodyParaer.urlencoded({
+//     extended:false,     //是否开启扩展
+//     limit:2*1024*1024   //传输大小
+// }))
+// server.use('/',function(req,res){
+//     console.log(req.body);
+// })
+
+//读取静态文件
+server.use(expressStatic('./www'))
