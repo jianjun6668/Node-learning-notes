ef22dec6148b0403973367f569af51f1fe8a312f
spring <jianjun6668@163.com>
2017-07-25 23:08:37 +0800

    第五天

---
+++ 第五天/server.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const cookieParse=require('cookie-parser');
+const cookirSession=require('cookie-session');
+
+var server=express();
+
+server.use(cookieParse());
+server.use(cookirSession({
+  keys:['aaa','bbb','ccc'], //传输数据加密
+  name:'nihao',   //传输的名字
+  maxAge:2*60*60*1000 //cookie存在的事件
+}))
+server.use('/aaa/a.html', function (req, res){
+  console.log(req.cookirsession) //客户端返回的数据
+  res.send({a:12}); //给客户端发送的数据
+});
+
+server.listen(8089);
--- 第四天/server.js
+++ 第四天/server.js
@@ -7,16 +7,18 @@ server.listen(8082);
 //get 请求
 server.use('/',function(req,res){
     console.log(req.query);
+    res.send({a:1,b:2});
 });
 
-//post 请求
-// server.use(bodyParaer.urlencoded({
-//     extended:false,     //是否开启扩展
-//     limit:2*1024*1024   //传输大小
-// }))
-// server.use('/',function(req,res){
-//     console.log(req.body);
-// })
+// post 请求
+server.use(bodyParaer.urlencoded({
+    extended:false,     //是否开启扩展
+    limit:2*1024*1024   //传输大小
+}))
+server.use('/',function(req,res){
+    console.log(req.body);
+    res.send({a:1,b:2});
+})
 
 //读取静态文件
 server.use(expressStatic('./www'))
